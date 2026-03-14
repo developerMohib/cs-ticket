d067ded93cf7ae93550392f8a194aaae2a39c042
developerMohib <mohibullahmohim2020@gmail.com>
2026-03-15 02:06:52 +0600

    Update banner with image

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,43 +1,69 @@
-import { tickets } from '../data/tickets';
+import { useEffect, useState } from 'react';
 import TicketCard from './TicketCard';
+import Loader from './Loader';
 
 const Task = () => {
+
+    const [tickets, setTickets] = useState([]);
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+        const fetchTickets = async () => {
+            const res = await fetch("/tickets.json");
+            setLoading(true)
+            const data = await res.json();
+            setTickets(data);
+            setLoading(false)
+        };
+
+        fetchTickets();
+    }, []);
+
+    const handleTicket = (id) => {
+        console.log(id)
+    }
+
+
+
+    if (loading) return <Loader />
     return (
         <div className='py-8'>
-            <h1>Customer Tickets</h1>
             <div className="grid grid-cols-12 gap-6">
-            {/* Ticket List */}
-            <div className="col-span-9 grid md:grid-cols-2 gap-4">
-                {tickets?.map(ticket => (
-                    <TicketCard key={ticket.id} ticket={ticket} />
-                ))}
-            </div>
-            {/* Task Status Panel */}
-            <div className="col-span-3 bg-white rounded-lg shadow p-4">
+                {/* Ticket List */}
+                <div className="col-span-9">
+                    <h1>Customer Tickets</h1>
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
 
-                <h2 className="font-semibold mb-4">Task Status</h2>
-
-                <div className="space-y-3">
-                    <div className="p-3 border rounded">
-                        <p className="text-sm mb-2">Payment Failed - Card Declined</p>
-                        <button className="bg-green-600 text-white w-full py-2 rounded">
-                            Complete
-                        </button>
-                    </div>
-
-                    <div className="p-3 border rounded">
-                        <p className="text-sm mb-2">Incorrect Billing Address</p>
-                        <button className="bg-green-600 text-white w-full py-2 rounded">
-                            Complete
-                        </button>
+                        {tickets?.map(ticket => (
+                            <TicketCard handleTicket={handleTicket} key={ticket.id} ticket={ticket} />
+                        ))}
                     </div>
                 </div>
+                {/* Task Status Panel */}
+                <div className="col-span-3 rounded-lg shadow px-4">
 
-                <h3 className="mt-6 font-semibold">Resolved Task</h3>
-                <p className="text-sm text-gray-400">No resolved tasks yet.</p>
+                    <h2 className="font-semibold mb-4">Task Status</h2>
 
+                    <div className="space-y-3">
+                        <div className="p-3 border rounded">
+                            <p className="text-sm mb-2">Payment Failed - Card Declined</p>
+                            <button className="bg-green-600 text-white w-full py-2 rounded">
+                                Complete
+                            </button>
+                        </div>
+
+                        <div className="p-3 border rounded">
+                            <p className="text-sm mb-2">Incorrect Billing Address</p>
+                            <button className="bg-green-600 text-white w-full py-2 rounded">
+                                Complete
+                            </button>
+                        </div>
+                    </div>
+
+                    <h3 className="mt-6 font-semibold">Resolved Task</h3>
+                    <p className="text-sm text-gray-400">No resolved tasks yet.</p>
+
+                </div>
             </div>
-        </div>
         </div>
     );
 };
--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -1,39 +1,45 @@
-const TicketCard = ({ ticket }) => {
+const TicketCard = ({ ticket, handleTicket }) => {
   return (
-    <div className="bg-white rounded-lg shadow p-4 border hover:shadow-md transition">
+    <button onClick={() => handleTicket(ticket.id)}>
+      <div className="rounded-lg shadow p-4 border hover:shadow-md bg-white transition">
 
-      <div className="flex justify-between items-start mb-2">
-        <h3 className="font-semibold text-gray-800">
-          {ticket.title}
-        </h3>
+        <div className="flex justify-between items-start mb-2">
+          <h3 className="font-semibold text-gray-800">
+            {ticket.title}
+          </h3>
 
-        <span className={`px-3 py-1 text-xs rounded-full
+          <p className={`px-3 py-1 text-xs rounded-full
           ${ticket.status === "Open" ? "bg-green-100 text-green-700" :
-          "bg-yellow-100 text-yellow-700"}`}>
-          {ticket.status}
-        </span>
-      </div>
+              "bg-yellow-100 text-yellow-700"}`}>
 
-      <p className="text-sm text-gray-500 mb-3 line-clamp-2">
-        {ticket.description}
-      </p>
+            <span
+              className={`inline-block w-2 h-2 rounded-full
+    ${ticket.status === "Open" ? "bg-green-500" : "bg-yellow-500"}
+  `}
+            ></span>
 
-      <div className="flex justify-between text-xs text-gray-400">
-        <span>{ticket.id}</span>
-        <span>{ticket.customer}</span>
-        <span>{ticket.createdAt}</span>
-      </div>
+{"  "}            {ticket.status} </p>
+        </div>
 
-      <p className={`text-xs mt-2 font-semibold
+        <p className="text-sm text-gray-500 mb-3 line-clamp-2 text-start">
+          {ticket.description}
+        </p>
+
+        <div className="flex justify-between text-xs text-gray-400 items-center">
+          <span>{ticket.id}</span>
+          <p className={`text-xs font-semibold
         ${ticket.priority === "HIGH" && "text-red-500"}
         ${ticket.priority === "MEDIUM" && "text-yellow-500"}
         ${ticket.priority === "LOW" && "text-green-500"}
       `}>
-        {ticket.priority} PRIORITY
-      </p>
-
-    </div>
+            {ticket.priority} PRIORITY
+          </p>
+          <span>{ticket.customer}</span>
+          <span> D {ticket.createdAt}</span>
+        </div>
+      </div>
+    </button>
   );
 };
 
-export default TicketCard ;
+export default TicketCard;
